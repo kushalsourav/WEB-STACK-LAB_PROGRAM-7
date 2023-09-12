4f641b65863d2c9a4a4d512eb3280482cf05623a
kushalsourav <kushalsourav2@gmail.com>
2023-09-12 21:46:05 +0530

    commit

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 const fetchbtn = document.getElementById('fetch-btn');
 const dataDisplay = document.getElementById('data');
 fetchbtn.addEventListener('click',async () => {
-
+    // const bookName = document.createElement('h1')
+    // const bookPrice = document.createElement('p')
+    // const bookDiv = document.createElement('div')
+    // bookDiv.classList.add('books')
+    // bookName.classList.add('book-name')
+    // bookPrice.classList.add('book-price')
     const books = await fetch('index.json').then(res => 
         res.json())
    books.books.map((book) => {
-    const bookName = document.createElement('h1')
-    const bookPrice = document.createElement('p')
-    const bookDiv = document.createElement('div')
-    bookDiv.classList.add('books')
-    bookName.classList.add('book-name')
-    bookPrice.classList.add('book-price')
-    bookName.append(book.name)
-    bookPrice.append(book.price)
-    bookDiv.append(bookName)
-    bookDiv.append(bookPrice)
-    dataDisplay.insertAdjacentElement('beforeend',bookDiv)
+    const ele = `<h1 class="book-name"> Name = ${book.name} </h2> <p class="book-price"> Price =  ${book.price} </p>`
+    dataDisplay.insertAdjacentHTML('beforeend',ele)
     })
     
     
